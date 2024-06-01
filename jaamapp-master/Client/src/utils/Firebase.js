39aa17987943fd5289913b439b8084e6743d6dbd
Dhanasriarul <dhanasri.arul02@gmail.com>
2024-06-01 20:14:42 +0530

    added files to env

--- jaamapp-master/Client/src/utils/Firebase.js
+++ jaamapp-master/Client/src/utils/Firebase.js
@@ -1,23 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {getAuth,GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 const firebaseConfig = {
-  apiKey: "AIzaSyBm_SUSiofNIqqAtKL977OgOg06Q9PWPAI",
-  authDomain: "subscription-stripe-api.firebaseapp.com",
-  projectId: "subscription-stripe-api",
-  storageBucket: "subscription-stripe-api.appspot.com",
-  messagingSenderId: "69809435373",
-  appId: "1:69809435373:web:766032eb2896f8da6112e4",
-  measurementId: "G-ZBPVEMTSE8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
+console.log("firbaseConfig: ", firebaseConfig)
 const app = initializeApp(firebaseConfig);
 export default app;
 
-const db=getFirestore();
-const auth=getAuth(app);
-const GoogleProvider=new GoogleAuthProvider();
+const db = getFirestore();
+const auth = getAuth(app);
+const GoogleProvider = new GoogleAuthProvider();
 
 
-export{db,auth,GoogleProvider};
+export { db, auth, GoogleProvider };
 
 // Import the functions you need from the SDKs you need
